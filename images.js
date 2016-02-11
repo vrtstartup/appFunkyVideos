59ea4258a0f10a16bf4b0496db47de0c107e32b2
eZ0 <kusksu@gmail.com>
2016-02-11 10:36:51 +0100

    download is not working - TODO

--- images.js
+++ images.js
@@ -35,20 +35,47 @@ router.post('/images', function(req, res, next) {
 //url /api
 router.get('/images', function(req, res) {
     // ffmpeg -i img%03d.jpeg -c:v libx264 -r 30 -pix_fmt yuv420p abc.mp4
+
+    var videoFileName = getDateTime();
+
     ffmpeg('temp/img%03d.jpeg')
         .fps(30)
         .on('end', function() {
-            var result = findRemoveSync('temp', { extensions: '.jpeg' });
+            var result = findRemoveSync('temp', { extensions: '.jpeg'});
             console.log('File has been converted succesfully');
         })
         .on('error', function(err) {
             console.log('Error: ' + err.message);
         })
         // save to file
-        .save('temp/abc.mp4');
+        .save('temp/videos/'+ videoFileName + '.mp4');
 
-        res.json({ url: 'temp/abc.mp4' });
+    res.json({ video_url: 'temp/videos/'+ videoFileName + '.mp4' });
 });
+
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return day + '.' + month + '.' + year + '-' + hour +  min + sec;
+}
 
 
 module.exports = router;
