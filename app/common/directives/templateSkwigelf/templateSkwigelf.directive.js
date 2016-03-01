dc5cac75c17b99b5d17a53c31a3875a099519275
eZ0 <kusksu@gmail.com>
2016-03-01 17:39:52 +0100

    switch for quote

--- app/common/directives/templateSkwigelf/templateSkwigelf.directive.js
+++ app/common/directives/templateSkwigelf/templateSkwigelf.directive.js
@@ -30,6 +30,8 @@ export const templateSkwigelfDirective = function() {
             authorName: '=',
             authorTitle: '=',
             isMirrored: '=',
+            isHiddenSign: '=',
+
         },
     };
 };
