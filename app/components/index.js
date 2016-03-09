89476f3cc3bd4741adf5b1d084c923363c119648
eZ0 <kusksu@gmail.com>
2016-03-09 10:27:36 +0100

    Moved chart animation into /chart

--- app/components/index.js
+++ app/components/index.js
@@ -2,10 +2,11 @@ import angular from 'angular';
 import home from './home';
 import subtitles from './subtitles';
 import pictures from './pictures';
+import chart from './chart';
 
 
 const module = angular.module('app.components', [
-    home, subtitles, pictures,
+    home, subtitles, pictures, chart,
 
 ]);
 
