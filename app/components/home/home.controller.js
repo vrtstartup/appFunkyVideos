3aece6e6d3a6ac8c1bdebf3a3371a6e38d558677
David De Keersmaecker <david.dekeersmaecker@euri.com>
2016-03-23 18:39:37 +0100

    home page tweaking

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -5,75 +5,74 @@ export default class HomeController {
         this.$scope = $scope;
         this.$timeout = $timeout;
 
-
-        this.tools = [
-
-   {
-                'image': 'assets/home-pictures.png',
-                'url': 'http://cryptic-everglades-93518.herokuapp.com/#/pictures',
+        this.tiles = [
+            {
                 'title': 'Afbeeldingmaker',
                 'sub': 'Maak snel afbeeldingen met voorgemaakte sjablonen',
-                'status': 'Klaar om te gebruiken, maar verwacht je aan een foutje hier en daar.'
+                'status': 'Klaar om te gebruiken, maar verwacht je aan een foutje hier en daar.',
+                'type': 'tool',
+                'category': 'facebook',
+                'image': 'assets/home-pictures.png',
+                'url': 'http://cryptic-everglades-93518.herokuapp.com/#/pictures'
             },
-
-            // {
-            //     'image': 'assets/home-subtitler.png',
-            //     'url': 'http://cryptic-everglades-93518.herokuapp.com/#/subtitles',
-            //     'title': 'Ondertitels',
-            //     'sub': 'Voeg ondertitels toe aan een filmpje',
-            //     'status': 'klaar om te gebruiken, maar verwacht je aan een foutje hier en daar.'
-            // },
-            // {
-            //     'image': 'assets/home-templater.png',
-            //     'url': 'https://vrtnieuwshub.firebaseapp.com/#/',
-            //     'title': 'Explainers',
-            //     'sub' : 'Voeg korte tekstje toe met uitleg aan een filmpje'
-            // },
             {
-                'image': 'assets/home-pagemaker.png',
-                'url': 'http://vrtstartup.github.io/vrtpagemaker',
+                'title': 'Ondertitels',
+                'sub': 'Voeg ondertitels toe aan een filmpje',
+                'status': 'klaar om te gebruiken, maar verwacht je aan een foutje hier en daar.',
+                'type': 'tool',
+                'category': 'facebook',
+                'image': 'assets/home-subtitler.png',
+                'url': 'http://cryptic-everglades-93518.herokuapp.com/#/subtitles'
+            },
+            {
+                'title': 'Explainers',
+                'sub': 'Voeg korte tekstje toe met uitleg aan een filmpje',
+                'type': 'tool',
+                'image': 'assets/home-templater.png',
+                'url': 'https://vrtnieuwshub.firebaseapp.com/#/'
+            },
+            {
                 'title': 'Pagemaker',
                 'sub': 'Maak een mooi, langer verhaal',
-                'status': 'Stuur een mailtje naar maarten.lauwaert@vrt.be indien je deze tool wil uittesten.'
+                'status': 'Stuur een mailtje naar maarten.lauwaert@vrt.be indien je deze tool wil uittesten.',
+                'type': 'tool',
+                'image': 'assets/home-pagemaker.png',
+                'url': 'http://vrtstartup.github.io/vrtpagemaker'
 
             },
-            //  {
-            //     'image': 'assets/home-charts.png',
-            //     'url': 'http://cryptic-everglades-93518.herokuapp.com/#/chart',
-            //     'title': 'Infografieken',
-            //     'sub': 'Maak bewegende infografieken',
-            //     'status': 'Voorlopig werkt slechts 1 template: het taartdiagram.'
-
-            // },
-
+            {
+                'title': 'Infografieken',
+                'sub': 'Maak bewegende infografieken',
+                'status': 'Voorlopig werkt slechts 1 template: het taartdiagram.',
+                'type': 'tool',
+                'image': 'assets/home-charts.png',
+                'url': 'http://cryptic-everglades-93518.herokuapp.com/#/chart'
+            },
+            {
+                'title': 'Facebook Algemeen',
+                'sub': 'Hoe werk je met Facebook?',
+                'type': 'guide',
+                'image': 'assets/home-facebook.png',
+                'url': 'https://app.frontify.com/d/1vi0ktgfFCVU/de-communicatiegids'
+            },
+            {
+                'title': 'Schrijven voor Facebook',
+                'sub': 'Hoe schrijf je best je posts op Facebook?',
+                'type': 'guide',
+                'image': 'assets/home-facebook.png',
+                'url': 'https://app.frontify.com/d/1DSJVfQzMjiF/facebook-tekst-en-stijlgids-style-guide'
+            },
+            {
+                'title': 'Handleiding pagemaker',
+                'sub': 'Uitgebreide handleiding om met de Pagemaker aan de slag te gaan',
+                'type': 'guide',
+                'image': 'assets/home-pagemakerGuide.png',
+                'url': 'https://app.frontify.com/d/TxcwgYOVtrNH/vrt-pagemaker-handleiding'
+            }
         ];
 
-
-        this.guides = [{
-            'image': 'assets/home-facebook.png',
-            'url': 'https://app.frontify.com/d/1vi0ktgfFCVU/de-communicatiegids',
-            'title': 'Facebook Algemeen',
-            'sub': 'Hoe werk je met Facebook?'
-        }, {
-            'image': 'assets/home-facebook.png',
-            'url': 'https://app.frontify.com/d/1DSJVfQzMjiF/facebook-tekst-en-stijlgids-style-guide',
-            'title': 'Schrijven voor Facebook',
-            'sub': 'Hoe schrijf je best je posts op Facebook?'
-        }, {
-            'image': 'assets/home-pagemakerGuide.png',
-            'url': 'https://app.frontify.com/d/TxcwgYOVtrNH/vrt-pagemaker-handleiding',
-            'title': 'Handleiding pagemaker',
-            'sub': 'Uitgebreide handleiding om met de Pagemaker aan de slag te gaan'
-        }];
-
-
-
+        this.filteredTiles = this.tiles;
     }
-
-    goTo(url) {
-        window.location.href = url;
-    }
-
 }
 
 HomeController.$inject = ['$log', '$http', '$scope', '$timeout'];
