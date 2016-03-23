c560445683f064ae20aa514b40a8b906a7117887
eZ0 <kusksu@gmail.com>
2016-03-23 15:07:50 +0100

    Templates + classes

--- app/common/directives/templateDethklok/templateDethklok.directive.js
+++ app/common/directives/templateDethklok/templateDethklok.directive.js
@@ -29,7 +29,7 @@ export const templateDethklokDirective = function() {
             isReady: '=',
             image: '=',
             templateClass: '=',
-            numbr: '=',
+            footerText: '=',
         },
     };
 };
