9179c5f41d875286d371f80eb03b4f06d63c8ea9
David De Keersmaecker <david.dekeersmaecker@euri.com>
2016-03-30 12:46:06 +0200

    fix disabled, fix links to docs

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -18,7 +18,8 @@ export default class HomeController {
                 'type': 'tool',
                 'image': 'assets/home-pagemaker.png',
                 'url': 'http://vrtstartup.github.io/vrtpagemaker', //URL to actual app - remove if only a guide
-                'docUrl': 'https://app.frontify.com/d/1vi0ktgfFCVU/de-communicatiegids' //URL to app manual or manual
+                'docUrl': 'https://app.frontify.com/d/1vi0ktgfFCVU/de-communicatiegids' //URL to app manual or manual,
+                'disabled': true
             },
          */
         this.tiles = [
@@ -38,42 +39,16 @@ export default class HomeController {
                 'category': 'facebook',
                 'image': 'assets/home-pictures.png',
                 'url': '/#/pictures',
-                'docUrl': 'https://app.frontify.com/document/80390#/foto-tools/headline'
+                'docUrl': 'https://app.frontify.com/d/1vi0ktgfFCVU/social-templates#/foto-tools/headline'
             },
             {
                 'title': 'Ondertitels',
                 'sub': 'Voeg ondertitels toe aan een filmpje',
-                'status': 'klaar om te gebruiken, maar verwacht je aan een foutje hier en daar.',
                 'type': 'tool',
                 'category': 'facebook',
                 'image': 'assets/home-subtitler.png',
                 'url': '/#/subtitles',
-                'docUrl': 'https://app.frontify.com/document/80390#/ondertiteling/ondertitels-toevoegen'
-            },
-            {
-                'title': 'Maps',
-                'sub': 'Genereer een map met marker',
-                'status': 'In ontwikkeling',
-                'type': 'tool',
-                'category': 'facebook',
-                'image': 'assets/home-maps.png',
-                'url': '/#/maps'
-            },
-            {
-                'title': 'Explainers',
-                'sub': 'Voeg korte tekstje toe met uitleg aan een filmpje',
-                'type': 'tool',
-                'image': 'assets/home-templater.png',
-                'url': 'https://vrtnieuwshub.firebaseapp.com/#/'
-            },
-            {
-                'title': 'Pagemaker',
-                'sub': 'Maak een mooi, langer verhaal',
-                'status': 'Stuur een mailtje naar maarten.lauwaert@vrt.be indien je deze tool wil uittesten.',
-                'type': 'tool',
-                'image': 'assets/home-pagemaker.png',
-                'url': 'http://vrtstartup.github.io/vrtpagemaker',
-                'docUrl': 'https://app.frontify.com/d/1vi0ktgfFCVU/de-communicatiegids'
+                'docUrl': 'https://app.frontify.com/d/1vi0ktgfFCVU/social-templates#/ondertiteling/ondertitels-toevoegen'
             },
             {
                 'title': 'Infografieken',
@@ -82,28 +57,55 @@ export default class HomeController {
                 'type': 'tool',
                 'image': 'assets/home-charts.png',
                 'url': '/#/chart',
-                'docUrl': 'https://app.frontify.com/document/80390#/video-tool/taartgrafiek'
+                'docUrl': 'https://app.frontify.com/d/1vi0ktgfFCVU/social-templates#/video-tool/taartgrafiek'
             },
             {
-                'title': 'Facebook Algemeen',
+                'title': 'Pagemaker',
+                'sub': 'Maak een mooi, langer verhaal',
+                'status': 'Stuur een mailtje naar maarten.lauwaert@vrt.be indien je deze tool wil uittesten.',
+                'type': 'tool',
+                'image': 'assets/home-pagemaker.png',
+                'url': 'http://vrtstartup.github.io/vrtpagemaker',
+                'docUrl': 'https://app.frontify.com/d/TxcwgYOVtrNH/vrt-pagemaker-handleiding#/de-basis/basis-in-7-regels'
+            },
+            {
+                'title': 'Maps',
+                'sub': 'Genereer een map met marker',
+                'status': 'In ontwikkeling',
+                'type': 'tool',
+                'category': 'facebook',
+                'image': 'assets/home-maps.png',
+                'url': '/#/maps',
+                'disabled': true
+            },
+            {
+                'title': 'Video met tekst',
+                'sub': 'Voeg korte tekstje toe met uitleg aan een filmpje',
+                'type': 'tool',
+                'image': 'assets/home-templater.png',
+                'url': 'https://vrtnieuwshub.firebaseapp.com/#/',
+                'disabled': true
+            },
+            {
+                'title': 'Facebook algemeen',
                 'sub': 'Hoe werk je met Facebook?',
                 'type': 'guide',
                 'image': 'assets/home-facebook.png',
                 'docUrl': 'https://app.frontify.com/d/1vi0ktgfFCVU/de-communicatiegids'
             },
+            //{
+            //    'title': 'Schrijven voor Facebook',
+            //    'sub': 'Hoe schrijf je best je posts op Facebook?',
+            //    'type': 'guide',
+            //    'image': 'assets/home-facebook.png',
+            //    'docUrl': 'https://app.frontify.com/d/1DSJVfQzMjiF/facebook-tekst-en-stijlgids-style-guide'
+            //},
             {
-                'title': 'Schrijven voor Facebook',
-                'sub': 'Hoe schrijf je best je posts op Facebook?',
-                'type': 'guide',
-                'image': 'assets/home-facebook.png',
-                'docUrl': 'https://app.frontify.com/d/1DSJVfQzMjiF/facebook-tekst-en-stijlgids-style-guide'
-            },
-            {
-                'title': 'Handleiding pagemaker',
+                'title': 'Pagemaker handleiding',
                 'sub': 'Uitgebreide handleiding om met de Pagemaker aan de slag te gaan',
                 'type': 'guide',
                 'image': 'assets/home-pagemakerGuide.png',
-                'docUrl': 'https://app.frontify.com/d/TxcwgYOVtrNH/vrt-pagemaker-handleiding'
+                'docUrl': 'https://app.frontify.com/d/TxcwgYOVtrNH/vrt-pagemaker-handleiding#/de-basis/basis-in-7-regels'
             }
         ];
 
