12a6b939345e78c5a27c389242c405c65ae095db
David De Keersmaecker <david.dekeersmaecker@euri.com>
2016-04-07 15:35:32 +0200

    fix more movie templater stuff

--- app/components/movies/movies.controller.js
+++ app/components/movies/movies.controller.js
@@ -11,55 +11,61 @@ export default class PicturesController {
         this.movieTypes = [
             {
                 'name': 'title',
-                'templateName': 'template_02_title'
+                'templateName': 'template_02_title',
+                'templateLocalPath': '/components/movies/movie.title.html'
             },
             {
                 'name': 'text-left',
-                'templateName': 'template_02_text_left'
+                'templateName': 'template_02_text_left',
+                'templateLocalPath': '/components/movies/movie.textleft.html'
             },
             {
                 'name': 'text-right',
-                'templateName': 'template_02_text_right'
+                'templateName': 'template_02_text_right',
+                'templateLocalPath': '/components/movies/movie.textright.html'
             },
             {
                 'name': 'quote',
-                'templateName': 'template_02_quote'
+                'templateName': 'template_02_quote',
+                'templateLocalPath': '/components/movies/movie.quote.html'
             },
             {
                 'name': 'number',
-                'templateName': 'template_02_number'
+                'templateName': 'template_02_number',
+                'templateLocalPath': '/components/movies/movie.number.html'
             },
             {
                 'name': 'bullet-list',
-                'templateName': 'template_02_bullet_list'
+                'templateName': 'template_02_bullet_list',
+                'templateLocalPath': '/components/movies/movie.bulletlist.html'
             },
         ];
 
-        initNewClip();
+        this.initMovie();
     }
 
     initMovie() {
         this.emailValid = true;
 
-        //#todo get movie ID, assign it to every clip
+        //#todo get movie ID, and assign it to clip
+        this.movie.id = 123;
+        this.initNewClip(this.movie.id);
     }
 
-    initNewClip() {
+    initNewClip(movieId) {
         this.currentClip = {
             'id': this.generateClipId(),
-            'bot': "render",
-            'render-status': "ready",
+            'movieId': movieId,
+            'bot': 'render',
+            'render-status': 'ready',
             'aep': 'filepath',
-            'movieId': '',
-            'last': ''
+            'last': '',
         }
     }
 
     addClip() {
-        console.log('currentClip', this.currentClip);
-        this.currentClip.id = generateClipId();
         this.movieClips.push(this.currentClip);
-        this.initNewClip();
+        this.initNewClip(this.movie.id);
     }
 
     deleteClip(clip) {
@@ -73,11 +79,17 @@ export default class PicturesController {
     }
 
     renderMovie() {
+        console.log('movie', this.movie);
+        console.log('movieClips', this.movieClips);
         //set 'last' to true for last clip in movieClips
+        //show confirm dialog
+        //get options and set them
 
         //send all clips to firebase
+        //https://vrtnieuwshub.firebaseio.com/apps/movies/movieclips
 
         //send movie to firebase
+        //https://vrtnieuwshub.firebaseio.com/apps/movies/movies
     }
 }
 
