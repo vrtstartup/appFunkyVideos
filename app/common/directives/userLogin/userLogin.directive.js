c65d2f90d961a7263594fb5ab7a20a3d7cb606fa
Maarten Lauwaert <maartenlauwaert@gmail.com>
2016-04-15 16:42:22 +0200

    Better intake and added activation step in register process

--- app/common/directives/userLogin/userLogin.directive.js
+++ app/common/directives/userLogin/userLogin.directive.js
@@ -10,14 +10,33 @@ class UserLoginDirectiveController {
         this.firebaseAuth = firebaseAuth;
         this.$firebaseObject = $firebaseObject;
         this.$firebaseArray = $firebaseArray;
+        this.authenticated = false;
+        this.accountVerified = false;
+        this.userExists = false;
+        this.userChecked = false;
+        this.correctDomain = false;
+        this.userId = '';
+        this.userForm = {};
+
 
         // Check if user is logged in. If not: open popup.
         this.firebaseAuth.$onAuth((authData) => {
             if (authData) {
+                console.log(authData);
+                this.authenticated = true;
                 this.userId = authData.uid;
+
+                let ref = new Firebase('https://vrtnieuwshub.firebaseio.com/users/' + this.userId);
+                let obj = this.$firebaseObject(ref);
+                console.log(obj);
+                // this.userForm.email = obj.email;
+
+                // obj.verificationStatus = 'verified';
+
+                this.checkAccountStatus(this.userId);
             } else {
                 this.userId = '';
-                this.openLoginPopup();
+                this.openLoginPopup(this.userForm);
             }
         });
     }
@@ -28,16 +47,67 @@ class UserLoginDirectiveController {
             password: user.password
         }).then((userData) => {
             this.message = "User logged in with uid: " + userData.uid;
+
             // If we logged in, set userId and close the popup.
-            this.userId = userData.uid;
+            // this.userId = userData.uid;
             this.closePopup();
         }).catch((error) => {
             this.error = error;
         });
     }
 
+
+    activateUser(user) {
+        this.firebaseAuth.$authWithPassword({
+            email: user.email,
+            password: user.oldPassword
+        }).then((userData) => {
+            this.setNewPassword(user);
+            this.closePopup();
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
     logOut() {
         this.firebaseAuth.$unauth();
+        this.userForm = {};
+        this.authenticated = false;
+        this.accountVerified = false;
+        this.userExists = false;
+        this.userChecked = false;
+        this.correctDomain = false;
+        this.userId = '';
+    }
+
+    setNewPassword(user) {
+        this.firebaseAuth.$changePassword({
+            email: user.email,
+            oldPassword: user.oldPassword,
+            newPassword: user.newPassword
+        }).then((data) => {
+            let ref = new Firebase('https://vrtnieuwshub.firebaseio.com/users/' + this.userId);
+            let obj = this.$firebaseObject(ref);
+            obj.verificationStatus = 'verified';
+            obj.email = user.email;
+            obj.$save().then((ref) => {
+                this.closePopup();
+            }, function(error) {
+                console.log("Error:", error);
+            });
+
+        }).catch(function(error) {
+            console.error("Error: ", error);
+        });
+    }
+
+    generatePassword() {
+        var possibleChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?_-';
+        var password = '';
+        for (var i = 0; i < 16; i += 1) {
+            password += possibleChars[Math.floor(Math.random() * possibleChars.length)]
+        }
+        return password;
     }
 
 
@@ -46,15 +116,24 @@ class UserLoginDirectiveController {
         if (domain === 'vrt.be') {
             this.firebaseAuth.$createUser({
                 email: user.email,
-                password: user.password
+                password: this.generatePassword()
             }).then((userData) => {
                 let ref = new Firebase('https://vrtnieuwshub.firebaseio.com/users/' + userData.uid);
                 let obj = this.$firebaseObject(ref);
-
                 obj.email = user.email;
-                obj.$save().then(function(ref) {
-                    // After we saved the user, log in.
-                    this.logIn(user);
+                obj.verificationStatus = 'pending';
+                obj.$save().then((ref) => {
+                    console.log(ref);
+                    console.log('reset password ');
+                    this.firebaseAuth.$resetPassword({
+                        email: user.email
+                    }).then(() => {
+                        console.log("Password reset email sent successfully!");
+                        // this.closePopup();
+                    }).catch(function(error) {
+                        console.error("Error: ", error);
+                    });
+
                 }, function(error) {
                     console.log("Error:", error);
                 });
@@ -67,11 +146,10 @@ class UserLoginDirectiveController {
     }
 
     closePopup() {
-
         // If the user is not logged in, do not make it possible to close the dialog.
         if (this.userId !== '') {
-
             this.$mdDialog.cancel();
+            this.user = {};
         } else {
             this.error = 'Je moet eerst inloggen.';
         }
@@ -90,20 +168,48 @@ class UserLoginDirectiveController {
     }
 
 
-    // If user exists: log in,
-    // if not: create.
-    logInOrRegister(user) {
-        // Check Firebase if user is already registered.
-        const ref = new Firebase('https://vrtnieuwshub.firebaseio.com/users/');
-        let query = ref.orderByChild('email').equalTo(user.email).on('value', (dataSnapshot) => {
-            if (dataSnapshot.val() !== null) {
-                // The user exists, so let's log in.
-                this.logIn(user);
-            } else {
-                // The user doesn't exist yet, so let's create a new user.
-                this.createUser(user);
-            }
-        });
+    checkAccountStatus(userId) {
+        let ref = new Firebase('https://vrtnieuwshub.firebaseio.com/users/' + userId);
+        let obj = this.$firebaseObject(ref);
+        obj.$loaded()
+            .then((data) => {
+                this.userForm.email = data.email;
+                if (data.verificationStatus === 'pending') {
+                    this.accountVerified = false;
+                    this.openLoginPopup(this.userForm);
+                } else {
+                    this.accountVerified = true;
+                }
+            })
+            .catch(function(error) {
+                console.error("Error:", error);
+            });
+    }
+
+
+    checkIfExistingUser(email) {
+        let domain = email.substring(email.lastIndexOf("@") + 1);
+        if (domain === 'vrt.be') {
+            this.correctDomain = true;
+            // Check Firebase if user is already registered.
+            let ref = new Firebase('https://vrtnieuwshub.firebaseio.com/users/');
+            let query = ref.orderByChild('email').equalTo(email).on('value', (snapshot) => {
+                if (snapshot.val() !== null) {
+                    let data = snapshot.val();
+                    let key = Object.keys(data)[0];
+                    this.userId = Object.keys(data)[0];
+                    this.userExists = true;
+                    this.userChecked = true;
+                    this.checkAccountStatus(this.userId);
+                } else {
+                    this.userExists = false;
+                    this.userChecked = true;
+                }
+            });
+        } else {
+            this.userChecked = true;
+            this.correctDomain = false;
+        }
     }
 }
 
