eb14ace8d353b931d827d7803fcdaaa246c6b15b
Maarten Lauwaert <maartenlauwaert@gmail.com>
2016-06-29 18:26:46 +0200

    Added options to burn without branding

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -48,10 +48,33 @@ function sendNotification(email, url) {
     var subject = 'Uw video met ondertitels is klaar om te downloaden!';
     var message = "<p>Beste collega,</p><p>Uw video met ondertitels is klaar, u kan hem hier downloaden:<br /> <a href=" + url +
         ">" + url + "</a></p><p>Nog een prettige dag verder,</p><p>De Hub Server</p>";
-
     emailService.sendMail(email, subject, message);
 }
 
+function sendResultToDropbox(video, videoName, ass, email) {
+    fs.readFile(video, function(err, data) {
+        if (!dbClient) {
+            console.log('No dbClient');
+        } else {
+            dbClient.writeFile('subtitled/' + videoName, data, function(error, stat) {
+                console.log('stat', stat);
+                if (error) {
+                    console.log('ERROR:', error);
+                    return next(Boom.badImplementation('unexpected error, couldn\'t upload file to dropbox'));
+                }
+                var fileUrl = 'subtitled/' + videoName;
+                dbClient.makeUrl(fileUrl, { downloadHack: true }, function(error, data) {
+                    console.log('data', data);
+                    sendNotification(email, 'https://www.dropbox.com/sh/cjfyjgupjbsdivh/AAB8zeW3Yc3cATefpKJV1ccha?dl=0');
+                    // Delete the tempVideo and the ass-File
+                    fs.unlinkSync(video);
+                    fs.unlinkSync(ass);
+                });
+            });
+        }
+    });
+
+}
 
 
 //url /api/movie
@@ -200,69 +223,144 @@ router.post('/burnSubs', function(req, res) {
     var height = req.body.height;
     var videoName = time() + '_' + (email.substring(0, email.indexOf("@"))).replace('.', '') + '.mp4';
     var tempVideo = path + videoName;
+
+
+
+
     // var ffmpegCommand = 'ffmpeg -i ' + movie + ' -i ' + bumper + ' -i ' + logo + ' -filter_complex "color=black:1920x1080:d=' + duration + '[base];[0:0]scale=1920:1080;[1:v]setpts=PTS-STARTPTS+((' + (duration - fade) + ')/TB)[bumper];[base][0:v]overlay[tmp];[tmp][bumper]overlay[allOverlayed];[allOverlayed]ass=' + ass + '[out]" -map [out] ' + tempVideo;
     // var ffmpegCommand = 'ffmpeg -i ' + movie + ' -i ' + bumper + ' -i ' + logo + ' -filter_complex "color=black:1920x1080:d=' + duration + '[base];[0:v]scale=1920:1080,setpts=PTS-STARTPTS;[1:v]setpts=PTS-STARTPTS+((' + (duration - fade) + ')/TB)[bumper];[base][v0]overlay[tmp];[tmp][bumper]overlay[allOverlayed],[allOverlayed]ass=' + ass + '[out]" -map [out] -map 0:1 -c copy -c:v libx264 -b:v 1000k ' + tempVideo;
     // var ffmpegCommand = 'ffmpeg -i ' + movie + ' -i ' + bumper + ' -i ' + logo + ' -filter_complex "color=black:1920x1080:d=' + duration + '[base];[0:v]setpts=PTS-STARTPTS[v0];[1:v]format=yuva420p,setpts=PTS-STARTPTS+((' + (duration - fade) + ')/TB)[v1];[base][v0]overlay[tmp];[tmp][v1]overlay,format=yuv420p[fv],[fv]ass=' + ass + '[sub],[sub][2:v]overlay=10:10[out]" -map [out] -c copy -c:v libx264 -b:v 1000k ' + tempVideo;
 
-    var ffmpegCommand = ffmpeg()
-        .input(movie)
-        .input(bumper)
-        .input(logo)
-        .complexFilter([
-            'color=black:' + width + 'x' + height + ':d=' + duration + '[blackVideo]',
-            '[0:v]setpts=PTS-STARTPTS[theMovie]',
-            '[1:v]scale=' + width + ':-1[bumperRescaled]',
-            '[bumperRescaled]format=yuva420p,setpts=PTS-STARTPTS+((' + (duration - fade) + ')/TB)[theBumper]',
-            '[2:v]scale=' + width / 5 + ':-1[logoRescaled]', {
-                filter: 'overlay',
-                options: { x: 0, y: 0 },
-                inputs: ['blackVideo', 'theMovie'],
-                outputs: 'longMovie'
-            }, {
-                filter: 'overlay',
-                options: { x: 10, y: 10 },
-                inputs: ['longMovie', 'logoRescaled'],
-                outputs: 'longMovieWithLogo'
-            }, {
-                filter: 'overlay',
-                options: { x: 0, y: 0 },
-                inputs: ['longMovieWithLogo', 'theBumper'],
-                outputs: 'longMovieWithLogoAndBumper'
-            },
-            '[longMovieWithLogoAndBumper]ass=' + ass + '[out]'
-        ], 'out')
-        .save(tempVideo)
-        .on('start', function(commandLine) {
-            console.log('Spawned Ffmpeg with command: ' + commandLine);
-        })
-        .on('error', function(err) {
-            console.log('An error occurred: ' + err.message);
-        })
-        .on('end', function() {
-            console.log('Processing finished !');
-            fs.readFile(tempVideo, function(err, data) {
-                if (!dbClient) {
-                    console.log('No dbClient');
-                } else {
-                    dbClient.writeFile('subtitled/' + videoName, data, function(error, stat) {
-                        console.log('stat', stat);
-                        if (error) {
-                            console.log('ERROR:', error);
-                            return next(Boom.badImplementation('unexpected error, couldn\'t upload file to dropbox'));
-                        }
-                        var fileUrl = 'subtitled/' + videoName;
-                        dbClient.makeUrl(fileUrl, { downloadHack: true }, function(error, data) {
-                            console.log('data', data);
-                            sendNotification(email, 'https://www.dropbox.com/sh/cjfyjgupjbsdivh/AAB8zeW3Yc3cATefpKJV1ccha?dl=0');
-                            // Delete the tempVideo and the ass-File
-                            fs.unlinkSync(tempVideo);
-                            fs.unlinkSync(ass);
-                        });
-                    });
-                }
-            });
 
-        })
+
+
+
+
+
+
+
+
+
+
+
+
+    if (bumper !== false && logo !== false) {
+
+        var ffmpegCommandBumperLogo = ffmpeg()
+            .input(movie)
+            .input(bumper)
+            .input(logo)
+            .complexFilter([
+                'color=black:' + width + 'x' + height + ':d=' + duration + '[blackVideo]',
+                '[0:v]setpts=PTS-STARTPTS[theMovie]',
+                '[1:v]scale=' + width + ':-1[bumperRescaled]',
+                '[bumperRescaled]format=yuva420p,setpts=PTS-STARTPTS+((' + (duration - fade) + ')/TB)[theBumper]',
+                '[2:v]scale=' + width / 5 + ':-1[logoRescaled]', {
+                    filter: 'overlay',
+                    options: { x: 0, y: 0 },
+                    inputs: ['blackVideo', 'theMovie'],
+                    outputs: 'longMovie'
+                }, {
+                    filter: 'overlay',
+                    options: { x: 10, y: 10 },
+                    inputs: ['longMovie', 'logoRescaled'],
+                    outputs: 'longMovieWithLogo'
+                }, {
+                    filter: 'overlay',
+                    options: { x: 0, y: 0 },
+                    inputs: ['longMovieWithLogo', 'theBumper'],
+                    outputs: 'longMovieWithLogoAndBumper'
+                },
+                '[longMovieWithLogoAndBumper]ass=' + ass + '[out]'
+            ], 'out')
+            .on('start', function(commandLine) {
+                console.log('Spawned Ffmpeg with command: ' + commandLine);
+            })
+            .on('error', function(err) {
+                console.log('An error occurred: ' + err.message);
+            })
+            .on('end', function() {
+                console.log('Processing finished !');
+                sendResultToDropbox(tempVideo, videoName, ass, email);
+
+            })
+            .save(tempVideo);
+    } else if (bumper !== false && logo === false) {
+        var ffmpegCommandBumper = ffmpeg()
+            .input(movie)
+            .input(bumper)
+            .complexFilter([
+                'color=black:' + width + 'x' + height + ':d=' + duration + '[blackVideo]',
+                '[0:v]setpts=PTS-STARTPTS[theMovie]',
+                '[1:v]scale=' + width + ':-1[bumperRescaled]',
+                '[bumperRescaled]format=yuva420p,setpts=PTS-STARTPTS+((' + (duration - fade) + ')/TB)[theBumper]', {
+                    filter: 'overlay',
+                    options: { x: 0, y: 0 },
+                    inputs: ['blackVideo', 'theMovie'],
+                    outputs: 'longMovie'
+                }, {
+                    filter: 'overlay',
+                    options: { x: 0, y: 0 },
+                    inputs: ['longMovie', 'theBumper'],
+                    outputs: 'longMovieWithBumper'
+                },
+                '[longMovieWithBumper]ass=' + ass + '[out]'
+            ], 'out')
+            .on('start', function(commandLine) {
+                console.log('Spawned Ffmpeg with command: ' + commandLine);
+            })
+            .on('error', function(err) {
+                console.log('An error occurred: ' + err.message);
+            })
+            .on('end', function() {
+                console.log('Processing finished !');
+                sendResultToDropbox(tempVideo, videoName, ass, email);
+
+            })
+            .save(tempVideo);
+    } else if (bumper === false && logo !== false) {
+        var ffmpegCommandLogo = ffmpeg()
+            .input(movie)
+            .input(logo)
+            .complexFilter([
+                '[1:v]scale=' + width / 5 + ':-1[logoRescaled]', {
+                    filter: 'overlay',
+                    options: { x: 10, y: 10 },
+                    inputs: ['[0]', 'logoRescaled'],
+                    outputs: 'movieWithLogo'
+                },
+                '[movieWithLogo]ass=' + ass + '[out]'
+            ], 'out')
+            .on('start', function(commandLine) {
+                console.log('Spawned Ffmpeg with command: ' + commandLine);
+            })
+            .on('error', function(err) {
+                console.log('An error occurred: ' + err.message);
+            })
+            .on('end', function() {
+                console.log('Processing finished !');
+                sendResultToDropbox(tempVideo, videoName, ass, email);
+            })
+            .save(tempVideo);
+
+    } else if (bumper === false && logo === false) {
+        var ffmpegCommandNoBranding = ffmpeg()
+            .input(movie)
+            .complexFilter([
+                '[0]ass=' + ass + '[out]'
+            ], 'out')
+            .on('start', function(commandLine) {
+                console.log('Spawned Ffmpeg with command: ' + commandLine);
+            })
+            .on('error', function(err) {
+                console.log('An error occurred: ' + err.message);
+            })
+            .on('end', function() {
+                console.log('Processing finished !');
+                sendResultToDropbox(tempVideo, videoName, ass, email);
+            })
+            .save(tempVideo);
+    }
 
 
 
