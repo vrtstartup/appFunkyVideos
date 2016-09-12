1be1d643b3a9edc782ae7fb1ea1b3058a6def120
Maarten Lauwaert <maarten.lauwaert@vrt.be>
2016-09-12 13:37:18 +0200

    Fixed dropbox path meta problem

--- app/components/subtitles/subtitles.controller.js
+++ app/components/subtitles/subtitles.controller.js
@@ -458,6 +458,7 @@ export default class SubtitlesController {
             })
             .then((resp) => {
                 // Set the meta information
+                this.meta.dropboxPath = resp.data.dbPath;
                 this.meta.movieName = resp.data.filenameIn;
                 this.meta.movieWidth = resp.data.width;
                 this.meta.movieHeight = resp.data.height;
