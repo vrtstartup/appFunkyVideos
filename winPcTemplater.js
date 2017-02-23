ca28d7840470f0d32ca5bcf81e917c6d6f18c8bb
Maarten Lauwaert <maarten.lauwaert@vrt.be>
2017-02-23 11:59:52 +0100

    commented out all logger refs

--- winPcTemplater.js
+++ winPcTemplater.js
@@ -3,33 +3,33 @@ var http = require('http');
 var lodash = require('lodash');
 var fs = require('fs');
 var winston = require('winston');
-var logger = new(winston.Logger)({
-    transports: [
-        new(winston.transports.Console)(),
-        new(winston.transports.File)({filename: 'logWinPcTemplaterScript.log'})
-    ]
+// var logger = new(winston.Logger)({
+transports: [
+new(winston.transports.Console)(),
+new(winston.transports.File)({ filename: 'logWinPcTemplaterScript.log' })
+]
 });
 
 var id = '';
 var movieId = '';
 var last = false;
 
-logger.info('-------------------------------------------------------');
-logger.info('running post-script, got following params', process.argv);
+// logger.info('-------------------------------------------------------');
+// logger.info('running post-script, got following params', process.argv);
 
 if (process.argv[2]) {
     id = process.argv[2];
-    logger.info('id', id);
+    // logger.info('id', id);
 }
 
 if (process.argv[3]) {
     movieId = process.argv[3];
-    logger.info('movie', movieId);
+    // logger.info('movie', movieId);
 }
 
 if (process.argv[4]) {
     last = process.argv[4];
-    logger.info('last', last);
+    // logger.info('last', last);
 }
 
 var data = querystring.stringify({
@@ -51,7 +51,7 @@ var options = {
 
 var req = http.request(options, function(res) {
     res.setEncoding('utf8');
-    res.on('data', function (chunk) {
+    res.on('data', function(chunk) {
         console.log("body: " + chunk);
     });
 });
@@ -61,7 +61,7 @@ req.end();
 
 //if this was the last fragment, send POST request to server
 if (last === "true") {
-    logger.info('this is the last file, sending request to render');
+    // logger.info('this is the last file, sending request to render');
 
     var data = querystring.stringify({
         movieId: movieId,
@@ -80,7 +80,7 @@ if (last === "true") {
 
     var req = http.request(options, function(res) {
         res.setEncoding('utf8');
-        res.on('data', function (chunk) {
+        res.on('data', function(chunk) {
             console.log("body: " + chunk);
         });
     });
